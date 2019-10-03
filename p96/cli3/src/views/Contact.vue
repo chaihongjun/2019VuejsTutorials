307c7549bdb190fdad8a7965e18aad9e23a79e2d
chaihongjun <441315628@qq.com>
2019-10-03 15:40:13 +0800

    vueRouter
    vueRouter

---
+++ p96/cli3/src/views/Contact.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: ChaiHongJun
+ * @Date: 2019-10-03 14:36:50
+ * @LastEditTime: 2019-10-03 15:17:11
+ * @LastEditors: ChaiHongJun
+ * @Description: 头部文件注释
+ -->
+<template>
+  <div class="contact">
+    <h1>This is an contact page</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "conract"
+};
+</script>
