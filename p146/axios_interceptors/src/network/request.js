00ce9d99e0301ebe64b1a4c2910e5c9da0f7afc0
chaihongjun <441315628@qq.com>
2019-10-18 13:41:57 +0800

    axios 拦截器
    axios 拦截器

--- p146/axios_interceptors/src/network/request.js
+++ p146/axios_interceptors/src/network/request.js
@@ -1,8 +1,33 @@
 /*
  * @Author: ChaiHongJun
  * @Date: 2019-10-17 15:24:48
- * @LastEditTime: 2019-10-17 15:24:48
+ * @LastEditTime: 2019-10-18 09:04:48
  * @LastEditors: ChaiHongJun
  * @Description: 头部文件注释
  */
-export function
+
+import axios from "axios";
+
+export function request(config) {
+  const instance = axios.create({
+    baseURL: "http://192.168.0.222:7300/mock/5d98362bb601f70efc7c1d7b",
+    timeout: 3000
+  });
+
+  //拦截发送请求
+  instance.interceptors.request.use(
+    config => {
+      // Do something before request is sent
+      console.log("interceptors");
+      console.log("config:" + config);
+      return config;
+    },
+    error => {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+
+  //返回 promise 对象
+  return instance(config);
+}
--- p146/axios_interceptors/src/views/About.vue
+++ p146/axios_interceptors/src/views/About.vue
@@ -1,5 +1,32 @@
+<!--
+ * @Author: ChaiHongJun
+ * @Date: 2019-10-17 15:23:10
+ * @LastEditTime: 2019-10-18 08:55:47
+ * @LastEditors: ChaiHongJun
+ * @Description: 头部文件注释
+ -->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import { request } from "@/network/request.js";
+
+export default {
+  name: "About",
+  created() {
+    request({
+      url: "/example/mock",
+      method: "get"
+    })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+</script>
